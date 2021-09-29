0efaa63ff950300d31bc02852d95ffb20b6b614d
martex84 <martex849@gmail.com>
2021-09-29 00:52:22 -0300

    Alteracao Index

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 import Server from './src/connectionServer.js';
+import database from './src/database/connection.js';
+import clientes from './src/model/clientes.js';
+import containers from './src/model/containers.js';
+import movimentacoes from './src/model/movimentacoes.js';
+
+const clintesModel = clientes; //Ira criar as telas caso as mesmas não estejam criadas antes de iniciar o server
+const containersModel = containers;
+const movimentacoesModel = movimentacoes;
 
 Server();
+
+await database.sync();
+
