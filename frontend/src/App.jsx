8168b7475378fd4992bb1675dda682ade573578c
martex84 <martex849@gmail.com>
2021-09-29 15:32:29 -0300

    Correcao arquivos padrao

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './css/App.css';
+
+import Header from './view/components/header';
+import Footer from './view/components/footer';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <section id="sectionPrincipal"></section>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
